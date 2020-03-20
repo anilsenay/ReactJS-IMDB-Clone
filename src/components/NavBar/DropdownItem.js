201590c597082db3d4f0a5bf2de3fa003061f62b
Anıl Şenay <anilsenay3@gmail.com>
2020-03-20 20:13:40 +0300

    just warming up

---
+++ src/components/NavBar/DropdownItem.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function DropdownItem(props) {
+    return (
+        <div>
+        {
+            props.items.map(item => {
+                return (
+                    <li key={item.title} className="nav-item dropdown my-sm-0 float-right">
+                        <a className="nav-link dropdown-toggle text-dark mr-5" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            {item.title}<i class="fas fa-chevron-down" style={{margin:"0px 12px"}}></i>
+                        </a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        {item.links.map(link => {
+                                return (
+                                    <a key={link.text} className="dropdown-item" href={link.url}>{link.text}</a>
+                                )
+                            })}
+                        </div>
+                    </li>
+                )
+            })
+        }
+        </div>
+    )
+}
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import logo from '../../img/logo.svg'
+import DropdownItem from './DropdownItem'
+
+const listItems = [
+    {
+        "title":"Movies",
+        "links":[
+            {
+                "text":"Top Rated Films",
+                "url": "#"
+            },
+            {
+                "text":"Most Popular Films",
+                "url": "#"
+            },
+            {
+                "text":"Top Box Office",
+                "url": "#"
+            }
+        ]
+    },
+    {
+        "title":"TV Shows",
+        "links":[
+            {
+                "text":"Top Rated Shows",
+                "url": "#"
+            },
+            {
+                "text":"Most Popular Shows",
+                "url": "#"
+            },
+            {
+                "text":"Browse TV Shows by Genre",
+                "url": "#"
+            }
+        ]
+    }
+]
+
+export default function NavBar() {
+    return (
+
+        <nav className="navbar navbar-expand-lg navbar-light bg-light navbar-custom"> 
+            <a className="navbar-brand" href="#">
+                <img src={logo} height="40" alt=""/>
+            </a>
+            <div className="collapse navbar-collapse float-right" id="navbarNavDropdown">
+                <ul className="navbar-nav">
+                    <DropdownItem items={listItems}/>
+                </ul>
+            </div>
+        </nav>
+
+    )
+}
