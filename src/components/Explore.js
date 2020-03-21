d382e93f8ba4b72389e687879ed4257b5af78478
Anıl Şenay <anilsenay3@gmail.com>
2020-03-22 02:47:08 +0300

    explore page getting datas

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -3,6 +3,12 @@ import ExploreCategory from './ExploreCategory'
 
 const filterCounter = 0
 
+const topMovies2019URL = "https://api.themoviedb.org/3/discover/movie?api_key=ee5e74e39e7bb0a1514fd8909bbd92f8&language=en-US&sort_by=vote_average.desc&page=1&primary_release_year=2019&vote_count.gte=2000&with_original_language=en"
+const trendingsURL = "https://api.themoviedb.org/3/trending/movie/week?api_key=ee5e74e39e7bb0a1514fd8909bbd92f8"
+const topRatedMoviesURL = "https://api.themoviedb.org/3/discover/movie?api_key=ee5e74e39e7bb0a1514fd8909bbd92f8&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&vote_count.gte=10000&with_original_language=en"
+const topRatedSeriesURL = "https://api.themoviedb.org/3/discover/tv?api_key=ee5e74e39e7bb0a1514fd8909bbd92f8&language=en-US&sort_by=vote_average.desc&page=1&vote_count.gte=500&include_null_first_air_dates=false"
+const topSeries2019URL = "https://api.themoviedb.org/3/discover/tv?api_key=ee5e74e39e7bb0a1514fd8909bbd92f8&language=en-US&sort_by=vote_average.desc&first_air_date_year=2019&page=1&vote_count.gte=500&include_null_first_air_dates=false"
+
 export default function Explore() {
 
     return (
@@ -13,9 +19,11 @@ export default function Explore() {
                 <span className="filter-counter">0</span>
                 <span className="font-weight-bold" style={{margin:"auto",marginLeft:"10px"}}>All Filters</span>
             </div>
-            <ExploreCategory title="Trendings"/>
-            <ExploreCategory title="Top Movies 2019"/>
-            <ExploreCategory title="Top Rated Movies"/>
+            <ExploreCategory title="Trendings" apiURL={trendingsURL}/>
+            <ExploreCategory title="Top Movies 2019" apiURL={topMovies2019URL}/>
+            <ExploreCategory title="Top Rated Movies" apiURL={topRatedMoviesURL}/>
+            <ExploreCategory title="Top TV Series 2019" apiURL={topSeries2019URL}/>
+            <ExploreCategory title="Top Rated TV Series" apiURL={topRatedSeriesURL}/>
 
         </div>
     )
--- src/components/ExploreCategory.js
+++ src/components/ExploreCategory.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class ExploreCategory extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            movies: []
+        }
+    }
+
+    componentDidMount() {
+    axios.get(this.props.apiURL)
+        .then(obj => {
+        const movies = obj.data.results.splice(0,4);
+        this.setState({ movies });
+        })
+    }
+
     render() {
+        console.log(this.state.movies)
         return (
             <div className="explore-category">
                 <span className="explore-title">{this.props.title}</span>
                 <div className="row explore-row">
-                    <div class="">
-                        <img src="https://image.tmdb.org/t/p/w300/kqjL17yufvn9OVLyXYpvtyrFfak.jpg" className = "explore-image" alt=""/>
-                    </div>
-                    <div class="">
-                        <img src="https://image.tmdb.org/t/p/w300/kqjL17yufvn9OVLyXYpvtyrFfak.jpg" className = "explore-image" alt=""/>
-                    </div>
-                    <div class="">
-                        <img src="https://image.tmdb.org/t/p/w300/kqjL17yufvn9OVLyXYpvtyrFfak.jpg" className = "explore-image" alt=""/>
-                    </div>
-                    <div class="">
-                        <img src="https://image.tmdb.org/t/p/w300/kqjL17yufvn9OVLyXYpvtyrFfak.jpg" className = "explore-image" alt=""/>
-                    </div>
+                    {
+                        this.state.movies.map(film => {
+                            return(
+                                <div key={film.title} class="">
+                                    <img src={`https://image.tmdb.org/t/p/w200`+film.poster_path} className = "explore-image" alt=""/>
+                                </div>
+                            )
+                        })
+                    }
+                    
                 </div>
-                <a href="/" style={{fontSize: "13pt", margin:"15px -5px", display: "inherit"}}>
-                    See All Trendings <i class="fas fa-angle-double-right" style={{color: "grey", fontSize:"10pt"}}></i>
+                <a href="/" style={{fontSize: "13pt", margin:"10px -5px", display: "inherit"}}>
+                    See All {this.props.title} <i class="fas fa-angle-double-right" style={{color: "grey", fontSize:"10pt"}}></i>
                 </a>
             </div>
         )
