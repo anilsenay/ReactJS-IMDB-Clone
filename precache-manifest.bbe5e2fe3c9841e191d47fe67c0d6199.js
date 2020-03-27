f0962c186e4ac6f9d15de911a9ddb3a2087ceb2b
Anıl Şenay <anilsenay3@gmail.com>
2020-03-27 19:23:49 +0300

    Updates

---
+++ precache-manifest.bbe5e2fe3c9841e191d47fe67c0d6199.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c25568d752ebba44dba5c687ce51d11",
+    "url": "/ReactJS-IMDB-Clone/index.html"
+  },
+  {
+    "revision": "8b2c0b0fac3e0392f02d",
+    "url": "/ReactJS-IMDB-Clone/static/css/main.c261b7e9.chunk.css"
+  },
+  {
+    "revision": "3977d44b10d3ac6d3dde",
+    "url": "/ReactJS-IMDB-Clone/static/js/2.fbade449.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactJS-IMDB-Clone/static/js/2.fbade449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b2c0b0fac3e0392f02d",
+    "url": "/ReactJS-IMDB-Clone/static/js/main.1a3a1728.chunk.js"
+  },
+  {
+    "revision": "34f4186a312805b527fb",
+    "url": "/ReactJS-IMDB-Clone/static/js/runtime-main.70dfaffb.js"
+  },
+  {
+    "revision": "51111366b238d2ee07fc33196be2038d",
+    "url": "/ReactJS-IMDB-Clone/static/media/logo.51111366.svg"
+  }
+]);
